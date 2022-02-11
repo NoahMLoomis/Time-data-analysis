419e587164cbf3bbf461f7450f5877d958a83e52
NoahMLoomis <Noahmmloomis@gmail.com>
2022-02-11 10:53:18 -0500

    Stashing: Bar chart now!

--- pages/api/data.js
+++ pages/api/data.js
@@ -2,6 +2,5 @@ import useFetch from "../hooks/useFetch"
 
 export default async function handler(req, res) {
     const data = await useFetch()
-    console.log(data)
     return res.status(200).json(data)
 }
--- pages/components/BarChart.js
+++ pages/components/BarChart.js
@@ -1,24 +1,86 @@
 import React from 'react';
 import * as d3 from 'd3';
+import useD3 from '../hooks/useD3'
 
-const BarChart = () => {
+const BarChart = ({ data }) => {
+    const ref = useD3(
+        (svg) => {
+
+            const height = 500;
+            const width = 500;
+            const margin = 50;
+
+
+            const x = d3
+                .scaleBand()
+                .domain(data.map((d) => d.Country))
+                .rangeRound([margin, width - margin])
+                .padding(0.5);
+
+            const y = d3
+                .scaleLinear()
+                .domain([0, d3.max(data, d => {
+                    console.log(d)
+                    return d["Time (minutes)"]
+                })])
+                .rangeRound([height - margin, margin]);
+
+            const xAxis = (g) =>
+                g.attr("transform", `translate(0,${height - margin})`).call(
+                    d3
+                        .axisBottom(x)
+                        .tickValues(
+                            d3
+                                .ticks(...d3.extent(x.domain()), width / 40)
+                                .filter((v) => x(v) !== undefined)
+                        )
+                        .tickSizeOuter(0)
+                );
+
+            const yAxis = (g) => g
+                .attr("transform", `translate(${margin},0)`)
+                .style("color", "steelblue")
+                .call(d3.axisLeft(y).ticks(null, "s"))
+                .call((g) => g.select(".domain").remove())
+                .call((g) => g
+                    .append("text")
+                    .attr("x", -margin)
+                    .attr("y", 10)
+                    .attr("fill", "currentColor")
+                    .attr("text-anchor", "start")
+                    .text(data.y)
+                );
+
+            svg.select(".x-axis").call(xAxis);
+            svg.select(".y-axis").call(yAxis);
+
+            svg
+                .select(".plot-area")
+                .attr("fill", "steelblue")
+                .selectAll(".bar")
+                .data(data)
+                .join("rect")
+                .attr("class", "bar")
+                .attr("x", (d) => x(d.Country))
+                .attr("width", x.bandwidth())
+                .attr("y", (d) => y(d["Time (minutes)"]))
+                .attr("height", (d) => y(0) - y(d["Time (minutes)"]));
+        },
+        [data.length]
+    )
+
     return (
         <svg
+            ref={ref}
             style={{
                 height: 500,
                 width: "100%",
-                marginRight: "0px",
-                marginLeft: "0px",
             }}
         >
             <g className="plot-area" />
-
             <g className="x-axis" />
-
             <g className="y-axis" />
-
         </svg>
-
     );
 }
 
