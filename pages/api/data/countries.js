b5dacc0229aeb3e5eeeedf2e139ea4dae20177df
NoahMLoomis <Noahmmloomis@gmail.com>
2022-02-15 11:48:04 -0500

    camcoins

--- pages/api/data/countries.js
+++ pages/api/data/countries.js
@@ -1,10 +1,6 @@
 import useAggregate from "../../hooks/useAggregate"
 
 export default async function Handler(req, res) {
-    let data = null
-    if (req.query) {
-        data = await useAggregate({ "Category": req.query.category }, { _id: "$Country", "time": { $sum: "$Time (minutes)" } }, {_id: 1})
-    } else
-        data = await useAggregate({}, { _id: "$Country", "time": { $sum: "$Time (minutes)" } })
+    data = await useAggregate({ "Category": req.query.category }, { _id: "$Country", "time": { $sum: "$Time (minutes)" } }, { _id: 1 })
     return res.status(200).json(data)
 }
