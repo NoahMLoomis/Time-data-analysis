1a75efabd68768d799c3ed39b9cd6d378d7dc2ae
NoahMLoomis <Noahmmloomis@gmail.com>
2022-02-18 10:55:20 -0500

    Stish staash i was taking a bath

--- pages/components/WorldMap.js
+++ pages/components/WorldMap.js
@@ -4,27 +4,27 @@ import {
     Geography,
     Sphere,
 } from "react-simple-maps";
-import { scaleLinear } from "d3-scale";
+import * as d3 from 'd3';
 import { Watch } from "react-loader-spinner";
-import { get } from "mongoose";
 
-const geoUrl =
-    "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+const geoUrl = "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
-const colorScale = scaleLinear()
-    .domain([0, 100])
-    .range(["#ffedea", "#ff5233"]);
 
 const WorldMap = ({ data }) => {
 
+    const colorScale = d3.scaleLinear()
+        .domain([d3.min(data.map(d => d.time)), d3.max(data.map(d => d.time))])
+        // .domain([d3.min(data.map(d => d.time)), d3.max(data.map(d => d.time))])
+        .range(["#ffedea", "#ff5233"]);
+
+    console.log([d3.min(data.map(d => d.time)), d3.max(data.map(d => d.time))])
     /**
-     * If you think this looks wrong, you're right.
+     * If you think this first return statement looks wrong, you're right.
      * The browser doesn't seem to like rendering the map at the proper size if you just return the <Watch /> component.
      * So in order to get the right sizing, I found you need to put an empty <ComposableMap />
      * to make sure when the map actually renders
      * the map is shown in the correct size
      *  */
-
     if (data.length <= 0) {
         return <>
             <Watch color="#0070f3" />
@@ -33,7 +33,6 @@ const WorldMap = ({ data }) => {
                     rotate: [-10, 0, 0],
                     scale: 147
                 }} />
-
         </>
     }
     return (
@@ -48,12 +47,8 @@ const WorldMap = ({ data }) => {
                 <Geographies geography={geoUrl}>
                     {({ geographies }) =>
                         geographies.map((geo) => {
-                            // As the DB is UK, but the stuff in the geo.properties.otherStuff doesn't match it.... i'm overriding the ISO_A3. But sketchy but it's fine
-                            if (geo.properties.NAME === "United Kingdom") {
-                                geo.properties.ISO_A3 = "UK"
-                            }
-                            const d = data.find((d) => d._id.toLowerCase() === geo.properties.NAME.toLowerCase() || d._id.toLowerCase() === geo.properties.ISO_A3.toLowerCase());
-                            return <Geography key={geo.rsmKey} geography={geo} fill={d ? colorScale(d.time) : "#F5F4F6"} stroke="black" strokeWidth={0.5} />
+                            const d = data.find((d) => d._id.toLowerCase() === geo.properties.NAME.toLowerCase() || d._id.toLowerCase() === geo.properties.NAME_LONG.toLowerCase());
+                            return <Geography key={geo.rsmKey} geography={geo} fill={d ? colorScale(d.time / 100) : "#F5F4F6"} stroke="black" strokeWidth={0.5} />
                         })
                     }
                 </Geographies>
