98ed0ef872cbc6fa5f73d50741cfcb68e85ac8f9
NoahMLoomis <Noahmmloomis@gmail.com>
2022-02-18 08:41:10 -0500

    Styling changes to the Map

--- pages/components/WorldMap.js
+++ pages/components/WorldMap.js
@@ -3,8 +3,6 @@ import {
     Geographies,
     Geography,
     Sphere,
-    Graticule,
-    ZoomableGroup
 } from "react-simple-maps";
 
 import { scaleLinear } from "d3-scale";
@@ -13,8 +11,8 @@ const geoUrl =
     "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
 const colorScale = scaleLinear()
-    .domain([0, 100])
-    .range(["orange", "red"]);
+    .domain([0.29, 0.68])
+    .range(["#ffedea", "#ff5233"]);
 
 const WorldMap = ({ data }) => {
     return (
@@ -25,13 +23,12 @@ const WorldMap = ({ data }) => {
             }}
         >
             <Sphere stroke="#E4E5E6" strokeWidth={0.5} />
-            <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
             {data.length > 0 && (
                 <Geographies geography={geoUrl}>
                     {({ geographies }) =>
                         geographies.map((geo) => {
                             const d = data.find((d) => d._id.toLowerCase() === geo.properties.NAME.toLowerCase());
-                            return <Geography key={geo.rsmKey} geography={geo} fill={d ? colorScale(d.time) : "black"} />
+                            return <Geography key={geo.rsmKey} geography={geo} fill={d ? colorScale(d.time) : "#F5F4F6"}  stroke="black" strokeWidth={0.5} />
                         })
                     }
                 </Geographies>
