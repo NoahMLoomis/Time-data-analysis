91d393efac80e9f674bf45751da901f53a448c71
Noah Loomis <nloomis@spiria.com>
2022-07-18 10:32:51 -0400

    asd

--- pages/components/RegionSection.js
+++ pages/components/RegionSection.js
@@ -13,7 +13,6 @@ const RegionSection = () => {
     useEffect(() => {
         fetch("/api/data/regions").then(d => d.json())
             .then(d => {
-                console.log(d)
                 setRegions([...new Set(d.map(item => item._id['region']))])
                 setCategoriesWithTime([...new Set(d.map(item =>
                     item._id['region'] === selectedRegion && { category: item._id['category'], averageTime: item.averageTime }
